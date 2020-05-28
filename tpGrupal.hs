15a317ea9f677203ed2c9a5d3a8e923e51ed3ef3
abas97 <abascalnicolas97@gmail.com>
2020-05-28 17:56:37 -0300

    Update tpGrupal.hs

--- tpGrupal.hs
+++ tpGrupal.hs
@@ -9,6 +9,7 @@ main = return()
 type Desgaste = Float
 type Patente = String
 type Fecha = (Float, Float, Float)
+type Tecnico = Auto->Auto
  
 -- Definiciones base
 anio :: Fecha -> Float
@@ -31,18 +32,18 @@ autoPrueba2 = Auto  "DJV214" [0.1, 0.4, 0.2, 0.1] 0 0 (20,10,2016)
 autoPrueba3 :: Auto
 autoPrueba3 = Auto  "DJV214" [0.1, 0.1, 0.1, 0] 0 0 (20,10,2016)
 
-buscarLetra:: Char->Bool
-buscarLetra 'D'=True
-buscarLetra 'J'=True
-buscarLetra 'N'=True
-buscarLetra 'B'=True
-buscarLetra _=False
+autoPrueba4 :: Auto
+autoPrueba4 = Auto  "DJV214" [0.1, 0.4, 0.2, 0.1] 0 0 (20,10,2016)
 
-cortarLetras :: Patente->String
-cortarLetras numeroPatente = filter buscarLetra numeroPatente
+autoPrueba5 :: Auto
+autoPrueba5 = Auto  "DJV214" [0.1, 0.4, 0.2, 0.1] 0 0 (20,10,2016)
+
+
+
+
 
 verificacionPatente :: Patente->Bool
-verificacionPatente numeroPatente= (( == "DJ").cortarLetras) numeroPatente || (( == "NB").cortarLetras) numeroPatente
+verificacionPatente numeroPatente= (take 2 numeroPatente) >= "DJ" && (take 2 numeroPatente) <= "NB"
 
 calculoPatental :: Patente -> Int
 calculoPatental numeroPatente |(( last numeroPatente) =='4')= (length numeroPatente)*3000
@@ -103,13 +104,9 @@ Dada una serie de autos, saber si están ordenados en base al siguiente criterio
 los autos ubicados en la posición impar de la lista deben tener una cantidad de desgaste impar
 los autos ubicados en la posición par deben tener una cantidad de desgaste par
 asumimos que el primer elemento está en la posición 1, el segundo elemento en la posición 2, etc.
-
 La cantidad de desgaste es la sumatoria de desgastes de las cubiertas de los autos multiplicada por 10. Ejemplo: 0.2 + 0.5 + 0.6 + 0.1 = 1.4 * 10 = 14. Para determinar si es par o no (y evitar errores de redondeo) es conveniente utilizar la función round.
-
 Solamente se puede utilizar recursividad en este punto.
-
 BONUS: Evitar repetición de código.
-
 Casos de prueba a definir
 Condición
 Qué se espera
@@ -122,16 +119,40 @@ Está ordenado según el criterio del enunciado
 Esta lista de autos: un auto con desgaste de cubiertas [0.1, 0.4, 0.2, 0.1]
 No está ordenado según el criterio del enunciado--}
 
-
+--4
 sumadeDesgaste :: [Auto] -> [Float]
 sumadeDesgaste []            = []
-sumadeDesgaste (cabeza:cola) = [((*10).sum.desgasteLlantas) cabeza] ++ suma cola
+sumadeDesgaste (cabeza:cola) = [((*10).sum.desgasteLlantas) cabeza] ++ sumadeDesgaste cola
 
 
-ordenamiento :: [Float]->Bool
-ordenamiento []= True
-ordenamiento lista | even ((round.head )lista) == (even.length)lista= ordenamiento (tail lista)
+ordenamiento :: Int->[Float]->Bool
+ordenamiento _ []= True
+ordenamiento n lista | even ((round.head )lista) == even (n+1) = ordenamiento (n+1) (tail lista)
                    |otherwise =False
 
 ordenamientoDeAutos :: [Auto]->Bool
-ordenamientoDeAutos lista = (ordenamiento.sumadeDesgaste) lista
+ordenamientoDeAutos lista = (ordenamiento 0.sumadeDesgaste) lista
+
+
+--5
+data OrdenDeReparacion = OrdenDeReparacion {
+     fechaOrden       :: Fecha,
+     trabajadores :: [Trabajo]
+} deriving (Show)
+
+aplicarOrdenDeReparacion :: OrdenDeReparacion -> Auto -> Auto
+aplicarOrdenDeReparacion (OrdenDeReparacion fecha [])  auto = cambiarFecha fecha auto
+aplicarOrdenDeReparacion (OrdenDeReparacion fecha (cabeza:cola)) auto = cambiarFecha fecha . cabeza . aplicarOrdenDeReparacion (OrdenDeReparacion fecha cola) $ auto
+
+cambiarFecha :: Fecha -> Auto -> Auto
+cambiarFecha unaFecha auto = auto { ultimoArreglo = unaFecha }
+
+--6
+tecnicoDejaAutoEnCondicion :: [Tecnico]->Auto->[Tecnico]
+tecnicoDejaAutoEnCondicion listaDeTecnicos auto= filter (reparaAuto auto) listaDeTecnicos
+
+reparaAuto :: Auto->Tecnico->Bool
+reparaAuto auto tecnico= (not.esAutoPeligroso.tecnico) auto
+
+costoReparacionAutos :: [Auto]->Int
+costoReparacionAutos listaAutos = (sum.map costoReparacion.filter necesitaRevision)listaAutos
